cefd7f0628199dd613a56d35ff6d2431a3958863
Andrew-R-Y <rakitinandrei@gmail.com>
2024-01-20 18:22:57 +0300

    fix: add try-catch block to 01-read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,9 +3,13 @@ const path = require('path');
 const filePath = path.join('./01-read-file', 'text.txt');
 const myStream = fs.createReadStream(filePath, 'utf8');
 async function myReadStreamFunction() {
-  await myStream.on('data', (result) => {
-    console.log(result);
-  });
+  try {
+    await myStream.on('data', (result) => {
+      console.log(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 myReadStreamFunction();
